d2a14d8f4547a9a7dd8debb19b30322fd54e5653
Aurelius Calliste <aureliuscalliste@aureliuss-mbp.home>
2020-10-05 16:08:02 +0100

    app-completion

--- WebTechTest-master/src/components/QuestionEditor/Question.js
+++ WebTechTest-master/src/components/QuestionEditor/Question.js
@@ -4,14 +4,23 @@ function Question() {
     return (
 
         <div>
-            <button id="submit-button">Question</button>
-            <div> <input id="input-1" type="text" placeholder="name" /></div>
+            <br />
+            <h3 className="question">Question</h3>
+            <br />
+            <div> <input id="input-1" type="text" placeholder="Edit Question..." /></div>
 
+            <br />
 
+            <h3 className="Answers">Answers</h3>
+            <br />
+            <input type="checkbox" id="checkbox2" name="checkbox" value="Bike"></input>
+            <div> <input id="input-2" type="text" placeholder="Edit Answer..." /></div>
+            <input type="checkbox" id="checkbox1" name="checkbox" value="Bike"></input>
 
-            <button id="next-question-button">Answers</button>
-            <div> <input id="input-2" type="text" placeholder="name" /></div>
-            <div> <input id="input-3" type="text" placeholder="name" /></div>
+            <br />  <br />
+            <p id="checkboxText">Is Correct?</p>
+            <div> <input id="input-3" type="text" placeholder="Edit Answer..." /></div>
+
 
         </div>
 
--- WebTechTest-master/src/components/QuestionEditor/index.tsx
+++ WebTechTest-master/src/components/QuestionEditor/index.tsx
@@ -1,7 +1,7 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import "./styles.css";
-import Checkbox from "./Checkbox"
+// import Checkbox from "./Checkbox"
 import Question from "./Question"
 
 
@@ -11,19 +11,8 @@ export default function QuestionEditor() {
     <Grid xs={7}>
 
       <h1>Editor (TODO)</h1>
-
-      <button id="submit-button">Question</button>
-      <div> <input id="input-1" type="text" placeholder="name" /></div>
-
-
-
-      <button id="next-question-button">Answers</button>
-      <div> <input id="input-2" type="text" placeholder="name" /></div>
-      <div> <input id="input-3" type="text" placeholder="name" /></div>
-
       <div>
         <Question />
-        <Checkbox />
       </div>
 
 
