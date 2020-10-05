aadc247796abb318bdfe5aac38198bfc4cf607e5
Aurelius Calliste <aureliuscalliste@aureliuss-mbp.home>
2020-10-05 14:41:35 +0100

    intial-ui-setup

--- WebTechTest-master/src/components/QuestionEditor/index.tsx
+++ WebTechTest-master/src/components/QuestionEditor/index.tsx
@@ -1,14 +1,29 @@
-import {  Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import React from "react";
+import "./styles.css";
 
 
 
-export default function QuestionEditor ()  {
+export default function QuestionEditor() {
 
-    return(
-        <Grid xs={7}>
-          <h1>Editor (TODO)</h1>
-        </Grid>
-      );
+  return (
+    <Grid xs={7}>
+
+      <h1>Editor (TODO)</h1>
+
+      <button id="submit-button">Question</button>
+      <div> <input id="input-1" type="text" placeholder="name" /></div>
+
+
+
+      <button id="next-question-button">Answers</button>
+      <div> <input id="input-2" type="text" placeholder="name" /></div>
+      <div> <input id="input-3" type="text" placeholder="name" /></div>
+
+
+
+
+    </Grid >
+  );
 
 }
